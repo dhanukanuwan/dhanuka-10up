fb906a6a991f573e57930f40842aa4405445e157
Dhanuka Gunarathna <nuwan.dhanuka@gmail.com>
2024-10-12 07:45:15 +0200

    generate global classes and styles using SASS

--- public/css/app.asset.php
+++ public/css/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '7ba9d3ad16d2c1ccf7a4');
+<?php return array('dependencies' => array(), 'version' => '57a8610fc4012befc616');
--- public/css/editor.asset.php
+++ public/css/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '7ba9d3ad16d2c1ccf7a4');
+<?php return array('dependencies' => array(), 'version' => '16c62a11e36f8de7d16b');
